9f3a62e2536f08a50c5b570dd6d47453ed7a15aa
Beloise Huffstutler <me@beloisehuffstutler.codes>
2022-06-28 13:04:11 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const axios = require("axios");
+const bodyParser = require("body-parser");
+const MongoClient = require('mongodb').MongoClient
+const cors = require('cors');
+const { resolveInclude } = require('ejs');
+const { response } = require('express');
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+require('dotenv').config();
+
+let db, dbCollection;
+
+const url = process.env.DB_STRING;
+const dbName = "raccoonplace";
+
+app.set("json spaces", 2);
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ encoded: true }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+
+app.listen(PORT, () => {
+    MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+        if(error) {
+            throw error;
+        }
+        db = client.db(dbName);
+        console.log("Connected to `" + dbName + "`!");
+    });
+})
+
+app.get("/", (req, res) => {
+    db.collection('raccoons').find().toArray((err, result) => {
+        if (err) return console.log(err)
+        let number = Math.floor(Math.random() * result.length)
+        console.log(result[number])
+        res.render('index.ejs', { raccoons: result[number] })
+    })
+})
+
+app.get("/getData", (req, res) => {
+    db.collection('raccoons').find().toArray((err, result) => {
+        if (err) return console.log(err)
+        res.json(result);
+    })
+})
+

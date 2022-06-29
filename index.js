4ba77f1c70ee37d99642ca2a4a6dbc294aa4f757
Beloise Huffstutler <me@beloisehuffstutler.codes>
2022-06-29 11:30:36 -0500

    Changing Port for Heroku

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const { resolveInclude } = require('ejs');
 const { response } = require('express');
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 80;
 const app = express();
 require('dotenv').config();
 

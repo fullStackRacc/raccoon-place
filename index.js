9077e7cc10eff8ef6cfe121fd499d3f9aa0efc31
Beloise Huffstutler <me@beloisehuffstutler.codes>
2022-06-29 11:05:09 -0500

    Pushing to Heroku

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const { resolveInclude } = require('ejs');
 const { response } = require('express');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 const app = express();
 require('dotenv').config();
 
